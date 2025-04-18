c3543b3881f14b69b7efb5f8d8f2bc606967c003
Yavuz Selim Gökdere <y.selim1029@gmail.com>
2025-04-18 19:31:16 +0300

    Projeler

--- responsiveDesign/script.js
+++ responsiveDesign/script.js
@@ -1,6 +1,7 @@
 function toggleMenu() {
     var menu = document.getElementById("mobileMenu");
-    if (menu.style.display === "block") {
+    var burger = document.getElementById("hamburger");
+    if (menu.style.display == "block") {
         menu.style.display = "none";
     } else {
         menu.style.display = "block";
