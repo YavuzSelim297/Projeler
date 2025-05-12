4b02b1b818275eda8f9ed9b4072d97d97c7cee2f
Yavuz Selim Gökdere <y.selim1029@gmail.com>
2025-05-12 22:58:11 +0300

    login+resign

--- responsiveDesign/script.js
+++ responsiveDesign/script.js
@@ -7,3 +7,36 @@ function toggleMenu() {
         menu.style.display = "block";
     }
 }
+
+function control() {
+    var isim  = document.getElementById("name").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var mesaj = document.getElementById("message").value.trim();
+
+    if (isim === "" || email === "" || mesaj === "") {
+        alert("Boş alan bırakmayınız!");
+        return;
+    }
+    document.getElementById("iletisimForm").submit();
+}
+
+function registerKontrol() {
+    var kullaniciAdi = document.getElementById("kullaniciAdi").value.trim();
+    var eposta = document.getElementById("Eposta").value.trim();
+    var sifre = document.getElementById("Sifre").value;
+    var sifreTekrar = document.getElementById("SifreTekrar").value;
+  
+    if (!kullaniciAdi || !eposta || !sifre || !sifreTekrar) {
+      alert("Lütfen tüm alanları doldurun.");
+      return;
+    }
+  
+    if (sifre !== sifreTekrar) {
+      alert("Şifreler uyuşmuyor.");
+      return;
+    }
+  
+    alert("Kayıt başarılı!");
+    document.getElementById("registerForm").submit();
+    window.location.href="index.html"
+  }
